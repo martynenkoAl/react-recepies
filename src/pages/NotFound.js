c507c65ebb806c628c4d5edc3c5e8bce2d6da12e
Alina Martynenko <martynenko.av.94@mail.ru>
2023-11-06 12:20:43 +1000

    change homepage

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -8,7 +8,7 @@ export default function NotFound() {
       <h1>404</h1>
       <h3>Страница не найдена</h3>
       <button
-        className='btn'
+        className='btn yellow darken-1 black-text'
         onClick={() => navigate(-1)}
         type='button'
         aria-label='Вернуться на предыдущую страницу'
